9f18c940d14b72cc4366f541e241844eefb22622
cheungje <cheungje@umich.edu>
2019-07-28 12:51:39 -0400

    Updates

---
+++ precache-manifest.b34c71db227c8e95b27641d3424d1b53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "458dc6c72fd7cc62d92e295c94070bec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "397d70967cf3726e50b4",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "381a11a9ce52e74dd659",
+    "url": "/static/js/2.cc1b1e2d.chunk.js"
+  },
+  {
+    "revision": "397d70967cf3726e50b4",
+    "url": "/static/js/main.804f44f1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
