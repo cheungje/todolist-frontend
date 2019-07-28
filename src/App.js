37c1a1901a2575859f1adcf33b1792ff376721ad
cheungje <cheungje@umich.edu>
2019-07-28 12:51:18 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import axios from "axios";
+import TaskItem from "./TaskItem.js";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = theme => ({
+  fab: {
+    right: 20,
+    bottom: 20,
+    position: 'fixed',
+    //when the position is fixed, then you can use right (20px from the right of the screen) and bottom 
+
+  }
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tasks: []
+    };
+
+    axios.get("http://api.ambrosia.red/tasks").then(res => {
+      this.setState({
+        tasks: res.data
+      });
+    });
+
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.handleAddTask = this.handleAddTask.bind(this);
+  }
+
+  //function that handles the update given a task passed in 
+  //task is a map 
+  handleUpdate(task) {
+    let updatedTasks = this.state.tasks;
+
+    for (var i = 0; i < updatedTasks.length; i++) {
+      if (updatedTasks[i].id === task.id) { //res.data = task = the entire task 
+        updatedTasks[i] = task;
+        break;
+      }
+    }
+
+    this.setState({
+      tasks: updatedTasks //tasks is now the new list of updated tasks 
+    });
+  }
+
+  handleAddTask() {
+    axios.post("http://api.ambrosia.red/tasks", {
+      due_date: "2019-07-14 01:29:03"
+    }).then(res => {
+      let updatedTasks = this.state.tasks;
+      updatedTasks.push(res.data);
+
+      this.setState({
+        tasks: updatedTasks
+      });
+    })
+  }
+
+
+  render() {
+    let taskElements = [];
+
+    for (var i = 0; i < this.state.tasks.length; i++) {
+      taskElements.push(
+        <TaskItem
+          key={this.state.tasks[i].id}
+          task={this.state.tasks[i]} //retrieves entire task with id, name, due_date, starred, notes
+          onUpdate={this.handleUpdate} />
+      );
+    }
+
+    const { classes } = this.props;
+
+    //render the list called taskElements
+    return (
+      <div className="App">
+        {taskElements}
+        <Fab 
+            color="primary" 
+            aria-label="add" 
+            className={classes.fab}
+            onClick={this.handleAddTask}
+            >
+          <AddIcon />
+        </Fab>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withStyles(useStyles)(App); 
---
+++ src/TaskItem.js
@@ -0,0 +1,236 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Checkbox from '@material-ui/core/Checkbox';
+import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
+import clsx from 'clsx';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import TextField from '@material-ui/core/TextField';
+import EventIcon from '@material-ui/icons/Event';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import MomentUtils from '@date-io/moment';
+import moment from 'moment';
+
+
+const useStyles = theme => ({
+  card: {
+    maxWidth: 580,
+    margin: "24px auto",
+    borderRadius: 3
+  },
+  cardContent: {  
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "flex-start",
+      fontFamily: "Helvetica Neue, sans-serif",
+      paddingBottom: 0
+  },
+  taskName : {
+    fontSize: 15
+  },
+  expand: {
+    transform: 'rotate(0)', //rotate 0 deg 
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)'
+  },
+  collapseContent: {
+    paddingTop: 0
+  },
+  dateTime: {
+    marginRight: 9,
+    width: 200,
+    textAlign: "right",
+    fontSize: 13
+  }
+});
+
+
+class TaskItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expanded: false,
+      notesValue: props.task.notes,
+      nameValue: props.task.name,
+      datePickerOpen: false,
+      dateValue: moment(props.task.due_date)
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDateToggle = this.handleDateToggle.bind(this);
+    this.handleExpandClick = this.handleExpandClick.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    this.handleNotesUpdate = this.handleNotesUpdate.bind(this);
+    this.handleNameUpdate = this.handleNameUpdate.bind(this);
+    this.handleFavoriteClick = this.handleFavoriteClick.bind(this);
+    this.handleDateAccept = this.handleDateAccept.bind(this);
+
+  }
+
+  handleExpandClick() {
+    this.setState ({
+      expanded: !this.state.expanded
+    })
+  }
+
+  handleChange() {
+    //axios.put(url, map of request body)
+    axios.put("http://api.ambrosia.red/tasks/" + this.props.task.id, {
+      completed: !this.props.task.completed //actual request body like {completed: true}
+    }).then(res => {
+      this.props.onUpdate(res.data); //res.data is "task," a map
+    });
+  }
+
+  handleBlur(e) {
+    axios.put("http://api.ambrosia.red/tasks/" + this.props.task.id, {
+      notes: this.state.notesValue, //get from reference called this.textInput
+      name: this.state.nameValue
+    }).then(res => {
+      this.props.onUpdate(res.data);
+    });
+  }
+
+  handleDateToggle(close) {
+    if (close) {
+      axios.put("http://api.ambrosia.red/tasks/" + this.props.task.id, {
+        //2019-07-14 01:29:03
+        due_date: this.state.dateValue.format("YYYY-MM-DD")
+      }).then(res => {
+        this.props.onUpdate(res.data);
+      })
+    }
+
+    this.setState({
+      datePickerOpen: !this.state.datePickerOpen,
+    });
+  }
+
+  handleDateAccept(date) {
+    this.setState({
+      dateValue: date
+    });
+  }
+
+  //e is the event, e.target is textfield 
+  handleNotesUpdate(e) {
+    this.setState({
+      notesValue: e.target.value
+    });
+  }
+
+  handleNameUpdate(e) {
+    this.setState({
+      nameValue: e.target.value
+    });
+  }
+
+  handleFavoriteClick() {
+    axios.put("http://api.ambrosia.red/tasks/" + this.props.task.id, {
+      starred: !this.props.task.starred 
+    }).then(res => {
+      this.props.onUpdate(res.data); //res.data is "task," a map
+    });
+
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    let icon;
+
+    if (this.props.task.starred) {
+      icon = <FavoriteIcon />;
+    } else {
+      icon = <FavoriteBorderIcon />;
+    }
+
+    return (
+      <Card className={classes.card}>
+        <CardContent className={classes.cardContent}>
+            <Checkbox className={classes.checkBox}
+                      checked={this.props.task.completed}
+                      onChange={this.handleChange} />
+            <TextField
+              fullWidth={true}
+              placeholder="New Task"
+              className={classes.textField}
+              value={this.state.nameValue}
+              onChange={this.handleNameUpdate}
+              onBlur={this.handleBlur}
+              InputProps={{disableUnderline: true}}
+            />
+            <Typography className={classes.dateTime}>
+              {/* {moment(this.props.task.due_date).format('ddd MMMM Do')} */}
+              {moment(this.state.dateValue).fromNow()}
+            </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+        <IconButton onClick={this.handleFavoriteClick}>
+            {icon}
+          </IconButton>
+          <IconButton onClick={() => this.handleDateToggle(false)}>
+            <EventIcon />
+          </IconButton>
+          <MuiPickersUtilsProvider utils={MomentUtils}>
+            <DatePicker
+              variant="inline"
+              value={this.state.dateValue}
+              InputProps={{disableUnderline: true, className: classes.dateTime}}
+              open={this.state.datePickerOpen}
+              onClose={() => this.handleDateToggle(true)}
+              onChange={this.handleDateAccept}
+            />
+          </MuiPickersUtilsProvider>
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: this.state.expanded,
+              })}
+            onClick={this.handleExpandClick}
+            aria-expanded={this.state.expanded}
+            aria-label="Show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+          <CardContent className={classes.collapseContent}>
+            <TextField
+              autoFocus={true}
+              fullWidth={true}
+              placeholder="Notes"
+              rows={2}
+              multiline={true}
+              className={classes.textField}
+              margin="normal"
+              value={this.state.notesValue}
+              onChange={this.handleNotesUpdate}
+              onBlur={this.handleBlur}
+              InputProps={{disableUnderline: true}}
+            />
+          </CardContent>
+        </Collapse>
+    </Card>
+    );
+  }
+}
+
+TaskItem.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(useStyles)(TaskItem); 
